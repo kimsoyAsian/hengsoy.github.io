9104f2388fc88c3218c5865f05e26d3fb092ad96
kimsoyAsian <115019654+kimsoyAsian@users.noreply.github.com>
2022-10-04 07:16:49 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+//selector
+const result = document.querySelector(".outputResult");
+const output = document.querySelector(".inputResult");
+const keys = document.querySelectorAll("button");
+keys.forEach(element => {
+    element.addEventListener("click", calculate)
+});
+
+
+function calculate(){
+    let buttonText = this.innerText;
+   
+    if(buttonText == "AC"){
+        output.innerText = ""
+        result.innerText = 0
+        return 
+    }
+    if(buttonText == "="){
+        result.innerText = eval(output.innerText)
+        return 
+    }
+    if(buttonText == "Del"){
+        output.textContent = output.textContent.substr(0,output.textContent.length-1)
+    
+        return
+    }
+    else{
+
+        output.textContent += buttonText;
+        return 
+    }
+    
+    
+}
